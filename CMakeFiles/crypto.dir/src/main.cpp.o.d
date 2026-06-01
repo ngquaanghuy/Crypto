d8c66a3ba7a4bd0db31ad093df2f043f7e928bc4
Quang Huy <ngquanghuy012@gmail.com>
2026-06-01 08:08:47 +0700

    tái cấu trúc project

--- CMakeFiles/crypto.dir/src/main.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/crypto.dir/src/main.cpp.o: \
- /home/ngquanghuy/Crypto/src/main.cpp /usr/include/stdc-predef.h \
- /home/ngquanghuy/Crypto/src/cli/cli.h \
- /home/ngquanghuy/Crypto/include/crypto/common.h \
- /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h
--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ngquanghuy/Crypto
-# Build directory: /home/ngquanghuy/Crypto
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_crypto "/home/ngquanghuy/Crypto/test_crypto")
-set_tests_properties(test_crypto PROPERTIES  _BACKTRACE_TRIPLES "/home/ngquanghuy/Crypto/CMakeLists.txt;101;add_test;/home/ngquanghuy/Crypto/CMakeLists.txt;0;")
